2d8be60ea81185a5cbd99a1a1931731531b66dfc
LuccaLu <luyangk@gmail.com>
2017-03-21 14:32:36 +0800

    test

--- index.php
+++ index.php
@@ -117,9 +117,9 @@ class JSSDK {
 $jssdk = new JSSDK("wxcac60c5221e2cc87", "b627222dbb85a7a10c541ff6420de033");
 $signPackage = $jssdk->GetSignPackage();
 
-$fptest = fopen("./jsapi_ticket.json", "w");
-fwrite($fptest, "test"));
-fclose($fptest);
+//$fptest = fopen("./jsapi_ticket.json", "w");
+//fwrite($fptest, "test"));
+//fclose($fptest);
 
 ?>
 
