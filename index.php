acf9814a385db27b230699d4681015e009cc1f32
LuccaLu <luyangk@gmail.com>
2017-03-21 15:23:37 +0800

    weixin debug

--- index.php
+++ index.php
@@ -151,6 +151,7 @@ $signPackage = $jssdk->GetSignPackage();
     <script>
 
         wx.config({
+            debug: true,
             appId: '<?php echo $signPackage["appId"];?>',
             timestamp: <?php echo $signPackage["timestamp"];?>,
             nonceStr: '<?php echo $signPackage["nonceStr"];?>',
